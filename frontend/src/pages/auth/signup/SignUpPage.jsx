498acb01fbc0f423f701eecf1248c6d9574e6577
adityaK486 <aditya201292@gmail.com>
2025-01-13 10:01:32 +0530

    signup to home redirect

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 import XSvg from "../../../components/svgs/X";
@@ -7,7 +7,7 @@ import { MdOutlineMail } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { MdPassword } from "react-icons/md";
 import { MdDriveFileRenameOutline } from "react-icons/md";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 const SignUpPage = () => {
@@ -17,6 +17,9 @@ const SignUpPage = () => {
 		fullName: "",
 		password: "",
 	});
+
+	const queryClient = useQueryClient();
+	const navigate = useNavigate();
 
 	const {mutate, isError, isPending, error} = useMutation({
 		mutationFn: async({email,username,fullName,password}) => {
@@ -31,15 +34,15 @@ const SignUpPage = () => {
 
 				const data = await res.json();
 				if(!res.ok) throw new Error(data.error||"Failed to create account");
-				console.log(data);
 				return data;
 			} catch (error) {
-				console.error(error);
 				throw error;
 			}
 		},
 		onSuccess:()=>{
 			toast.success("Account created successfully");
+			queryClient.invalidateQueries({queryKey:["authUser"]});
+			navigate("/");
 		},
 	});
 
